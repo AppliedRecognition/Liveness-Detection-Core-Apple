c8f9cc500e8f62a58b758ab83e559e361662a104
Jakub Dolejs <jakubdolejs@gmail.com>
2024-05-10 21:19:17 +0100

    Removed logger, added documentation

---
+++ Sources/LivenessDetection/LivenessDetection.docc/SpoofDetector.md
@@ -0,0 +1,17 @@
+# ``SpoofDetector``
+
+## Overview
+
+Protocol adopted by all Ver-ID spoof detectors
+
+## Topics
+
+### Detecting spoofs
+
+- ``SpoofDetector/detectSpoofInImage(_:regionOfInterest:)``
+- ``SpoofDetector/isSpoofImage(_:regionOfInterest:)``
+
+### Detector properties
+
+- ``SpoofDetector/identifier``
+- ``SpoofDetector/confidenceThreshold``
--- Sources/LivenessDetection/SpoofDetector.swift
+++ Sources/LivenessDetection/SpoofDetector.swift
@@ -8,20 +8,38 @@
 import Foundation
 import UIKit
 
+/// Spoof detector protocol
+/// - Since: 1.0.0
 public protocol SpoofDetector {
     
+    /// Spoof detector identifier
+    /// - Since: 1.0.0
     var identifier: String { get }
     
+    /// Confidence threshold
+    ///
+    /// Detections with scores below the threshold are not treated as spoofs
+    /// - Since: 1.0.0
     var confidenceThreshold: Float { get set }
     
-    var logger: Logger { get set }
-    
+    /// Detect spoof in image
+    /// - Parameters:
+    ///   - image: Image in which to detect spoof
+    ///   - roi: Region of interest – this is typically a face bounding box
+    /// - Returns: Confidence score between `0.0` and `1.0`
+    /// - Since: 1.0.0
     func detectSpoofInImage(_ image: UIImage, regionOfInterest roi: CGRect?) async throws -> Float
 }
 
 public extension SpoofDetector {
     
-    func isSpoofedImage(_ image: UIImage, regionOfInterest roi: CGRect?) async throws -> Bool {
+    /// Check whether image is a spoof
+    /// - Parameters:
+    ///   - image: Image to check for spoofs
+    ///   - roi: Region of interest – this is typically a face bounding box
+    /// - Returns: `true` if the image is a spoof, `false` otherwise
+    /// - Since: 1.0.0
+    func isSpoofImage(_ image: UIImage, regionOfInterest roi: CGRect?) async throws -> Bool {
         let score = try await self.detectSpoofInImage(image, regionOfInterest: roi)
         return score >= self.confidenceThreshold
     }
